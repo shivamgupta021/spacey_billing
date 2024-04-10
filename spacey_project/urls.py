873313c0c8e6cef56af5ab08f488d9651f5d0934
Shivam Gupta <shivam.gupta02210@gmail.com>
2024-04-10 21:59:42 +0530

    spectacular options
    Signed-off-by: Shivam Gupta <shivam.gupta02210@gmail.com>

--- spacey_project/urls.py
+++ spacey_project/urls.py
@@ -17,14 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("billing.api.urls")),
     path("api/schema", SpectacularAPIView.as_view(), name="schema"),
-    path("api/schema/docs", SpectacularSwaggerView.as_view(url_name="schema")),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
